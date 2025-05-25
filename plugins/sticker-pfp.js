c223c1193af2ce34e63929d0ee5188502e408c7a
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-05-24 17:10:16 -0700

    Actualizar sticker-pfp.js

--- plugins/sticker-pfp.js
+++ plugins/sticker-pfp.js
@@ -4,7 +4,7 @@
 
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let name = conn.getName(who);
+    let name = await conn.getName(who); // Corrección: ahora se usa await
     let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg');
     await conn.sendFile(m.chat, pp, 'profile.jpg', `*Foto de perfil de ${name}*`, m);
 }
