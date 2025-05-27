6349a8e65fea6c3a5e569fd43f0a250e2aae7dd7
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-05-27 00:13:40 -0700

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -64,3 +64,4 @@ export default handler;
 
 const isUrl = (text) => {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'));
+};
