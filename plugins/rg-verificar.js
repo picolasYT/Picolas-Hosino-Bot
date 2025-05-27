4777da979064274f9f3a07c93e5efad4ddda0e29
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-05-27 00:07:37 -0700

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -67,7 +67,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
         renderLargerThumbnail: true,
       }
     }
-  }, { quoted: fkontak })
+  }, { quoted: m })
 }
 
 handler.help = ['reg']
