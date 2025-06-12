45266048c8172c6f7f12501c1eb1f05711669a0d
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-06-11 20:07:03 -0700

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let img = './src/catalogo.jpg'
 let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
-✰ *Dueño* Dioneibi-rip
-✦ *Bot:* ׄ❀ׅᮢ໋۬۟   ׁ ᮫᩠𝗥ᥙ᜔᪲𝖻ֹ𝘺 𝐇֢ᩚᨵ̷̸ׁׅׅ𝗌𝗁𝗂ᮬ𝗇֟፝͡𝗈̷̸  ꫶֡ᰵ࡙🌸̵໋ׄᮬ͜✿֪
-⚘ *Versión:* ${vs}
-❖ *Libreria:* ${libreria} ${baileys}
+✰ *Dueño* » Dioneibi-rip
+✦ *Bot* » ׄ❀ׅᮢ໋۬۟   ׁ ᮫᩠𝗥ᥙ᜔᪲𝖻ֹ𝘺 𝐇֢ᩚᨵ̷̸ׁׅׅ𝗌𝗁𝗂ᮬ𝗇֟፝͡𝗈̷̸  ꫶֡ᰵ࡙🌸̵໋ׄᮬ͜✿֪
+⚘ *Versión* » ${vs}
+❖ *Libreria* » ${libreria} ${baileys}
 > ✧ GitHub » https://github.com/Dioneibi-rip
 
-✰ *Colaborador 1*: Nevi 
-✦ *Rol*: Ayudante y desarrollador. 
-> ✧ Github »
+✰ *Colaborador 1* » Nevi 
+✦ *Rol* » Ayudante y desarrollador. 
+> ✧ Github » https://github.com/nevi-dev
 
-✰ *Colaborador 2*: Legna 
-✦ *Rol*: Soporte/ayudante,editor.
-> ✧ Github »
+✰ *Colaborador 2* » Legna 🅜
+✦ *Rol* » Soporte/ayudante,editor.
+> ✧ Github » https://github.com/Legna-chan
 `
 await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), m)
 }
