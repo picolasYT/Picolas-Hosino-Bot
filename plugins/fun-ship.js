1034f08d90c9860a18cb941030049ac4b4c26a49
Dioneibi-rip <fandubdreams@gmail.com>
2025-06-15 21:58:54 -0400

    Actualizar fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -1,19 +1,42 @@
-var handler = async (m, { conn, command, text }) => {
+import fetch from 'node-fetch';
 
-if (!text) return conn.reply(m.chat, `${emoji} Escribe tu nombre y el nombre de la otra personas para calcular su amor.`, m)
-let [text1, ...text2] = text.split(' ')
+var handler = async (m, { conn, args, participants }) => {
+  let user1, user2;
 
-text2 = (text2 || []).join(' ')
-if (!text2) return conn.reply(m.chat, `${emoji2} Escribe el nombre de la segunda persona.`, m)
-let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de ${Math.floor(Math.random() * 100)}% 👩🏻‍❤️‍👨🏻`
+  if (m.mentionedJid.length === 2) {
+    user1 = m.mentionedJid[0];
+    user2 = m.mentionedJid[1];
+  } else if (m.mentionedJid.length === 1) {
+    user1 = m.sender;
+    user2 = m.mentionedJid[0];
+  } else {
+    return conn.reply(m.chat, `❤️ Menciona a una o dos personas para shippearlas.\n\nEjemplo:\n.ship @usuario1 @usuario2`, m);
+  }
 
-m.reply(love, null, { mentions: conn.parseMention(love) })
+  const name1 = await conn.getName(user1);
+  const name2 = await conn.getName(user2);
+  const avatar1 = await conn.profilePictureUrl(user1, 'image').catch(_ => 'https://i.ibb.co.com/Yc4MVdV/images.jpg');
+  const avatar2 = await conn.profilePictureUrl(user2, 'image').catch(_ => 'https://i.ibb.co.com/KKYxYQr/download.jpg');
 
-}
-handler.help = ['ship', 'love']
-handler.tags = ['fun']
-handler.command = ['ship','pareja']
+  const lovePercent = Math.floor(Math.random() * 101);
+  const background = encodeURIComponent('https://i.ibb.co/4YBNyvP/images-76.jpg');
+  const api = `https://api.siputzx.my.id/api/canvas/ship?avatar1=${encodeURIComponent(avatar1)}&avatar2=${encodeURIComponent(avatar2)}&background=${background}&persen=${lovePercent}`;
+
+  let loveMessage = `
+💖 *Compatibilidad del amor entre ${name1} y ${name2}* 💖
+❤️ *Resultado:* ${lovePercent}%
+💌 *Pareja potencial:* ${name1} 💞 ${name2}
+`.trim();
+
+  await conn.sendFile(m.chat, api, 'ship.jpg', loveMessage, m, false, {
+    mentions: [user1, user2]
+  });
+};
+
+handler.help = ['ship @user1 @user2'];
+handler.tags = ['fun'];
+handler.command = ['ship', 'pareja'];
 handler.group = true;
-handler.register = true
+handler.register = true;
 
-export default handler
+export default handler;
