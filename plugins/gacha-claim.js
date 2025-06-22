01c9263cd7a30bb2dd1497e8c8f114dc313356ac
Dioneibi-rip <fandubdreams@gmail.com>
2025-06-22 00:10:06 -0400

    Actualizar gacha-claim.js

--- plugins/gacha-claim.js
+++ plugins/gacha-claim.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn }) => {
     if (m.quoted && m.quoted.text) {
         try {
             const characters = await loadCharacters();
-            const characterIdMatch = m.quoted.text.match(/ID: \*(.+?)\*/);
+            const characterIdMatch = m.quoted.text.match(/𝙄𝘿: \*(.+?)\*/);
 
             if (!characterIdMatch) {
                 await conn.reply(m.chat, '《✧》No se pudo encontrar el ID del personaje en el mensaje citado.', m);
