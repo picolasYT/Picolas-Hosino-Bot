236aa8df0164681a789069882ace1e373e8fca81
Dioneibi-rip <fandubdreams@gmail.com>
2025-06-27 12:20:31 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -140,9 +140,8 @@ global.readMore = more.repeat(850)
 global.packsticker = `⏜͜⌒᳝︵໋۪۪۪۪۪᳝֔࣪⡳۟︵໋۪۪۪᳝֔࣪⌒꒰🍥꒱︵᳝࣪⡳۪۟︵໋۪۪۪᳝֔⌒⏜
 ⌯⃝⃕ 🧸 𝐩𝐫𝐨𝐩𝐢𝐞𝐝𝐚𝐝 𝐝𝐞 ${nombre}
 
-> 𑁯ᰰᘘ᷒ᰰ ༝⃟ꪆ 𝙂𝘼𝙔 𝙎𝙄 𝙇𝙊 𝙍𝙊𝘽𝘼𝙎 ༝⃟ꪆ ᘘ᷒ᰰ𑁯`;
+ 𑁯ᰰᘘ᷒ᰰ ༝⃟ꪆ 𝙂𝘼𝙔 𝙎𝙄 𝙇𝙊 𝙍𝙊𝘽𝘼𝙎 ༝⃟ꪆ ᘘ᷒ᰰ𑁯`;
 
-// rcanaɭ con icono como buffer
 global.rcanal = {
   contextInfo: {
     isForwarded: true,
