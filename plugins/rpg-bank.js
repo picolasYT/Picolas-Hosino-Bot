b36018aed9efde523ae0bec91c75e0a4947f7c5c
Dioneibi-rip <fandubdreams@gmail.com>
2025-06-29 22:31:41 -0400

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -2,43 +2,39 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix }) => {
     let who = m.mentionedJid[0] 
-            ? m.mentionedJid[0] 
-            : m.quoted 
-            ? m.quoted.sender 
-            : m.sender
+        ? m.mentionedJid[0] 
+        : m.quoted 
+        ? m.quoted.sender 
+        : m.sender
 
-    if (who === conn.user.jid) return m.react('✖️')
-    if (!(who in global.db.data.users)) {
-        return m.reply(`${emoji} El usuario no se encuentra en mi base de datos.`)
-    }
+    if (who == conn.user.jid) return m.react('✖️')
+    if (!(who in global.db.data.users)) 
+        return m.reply(`${emoji} *El usuario no se encuentra en mi base de datos.*`)
 
     let user = global.db.data.users[who]
-    let name = await conn.getName(who)
+    let nombre = await conn.getName(who)
 
-    let dinero = (user.coin || 0).toLocaleString()
-    let banco = (user.bank || 0).toLocaleString()
-    let total = ((user.coin || 0) + (user.bank || 0)).toLocaleString()
+    let coin = (user.coin || 0).toLocaleString('en-US')
+    let bank = (user.bank || 0).toLocaleString('en-US')
+    let total = ((user.coin || 0) + (user.bank || 0)).toLocaleString('en-US')
 
     let texto = `
-┏━━━━━━━━━━━━━━━━━━⬣
-┃ 𓆩⟡𖥔 𝐁𝐀𝐋𝐀𝐍𝐂𝐄 𝐄𝐂𝐎𝐍𝐎́𝐌𝐈𝐂𝐎 𖥔⟡𓆪
-┃     
-┃ 🧑‍💼 Usuario: *${name}*
-┃ 💸 Dinero: *¥${dinero} Yenes*
-┃ 🏦 Banco: *¥${banco} Yenes*
-┃ 💰 Total: *¥${total} Yenes*
-┃     
-┃ 🪙 Protege tu dinero, deposítalo
-┃ usando *${usedPrefix}deposit*
-┗━━━━━━━━━━━━━━━━━━⬣`.trim()
+╭─〔 ᥫ᭡ 𝗜𝗡𝗙𝗢 𝗘𝗖𝗢𝗡𝗢́𝗠𝗜𝗖𝗔 ❀ 〕
+│ 👤 Usuario » *${nombre}*
+│ 💸 Dinero » *¥${coin} ${moneda}*
+│ 🏦 Banco » *¥${bank} ${moneda}*
+│ 🧾 Total » *¥${total} ${moneda}*
+╰─────────────────────
+> 📌 Usa *${usedPrefix}deposit* para proteger tu dinero en el banco.
+    `.trim()
 
     await conn.reply(m.chat, texto, m)
 }
 
 handler.help = ['bal']
 handler.tags = ['rpg']
-handler.command = ['bal', 'balance', 'bank'] 
-handler.register = true 
-handler.group = true 
+handler.command = ['bal', 'balance', 'bank']
+handler.register = true
+handler.group = true
 
 export default handler
