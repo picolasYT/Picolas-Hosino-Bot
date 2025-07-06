7b5751043d83c0c43c5409ccac1651c5b27f02c4
Dioneibi-rip <fandubdreams@gmail.com>
2025-07-06 13:13:44 -0400

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,63 +1,38 @@
-import { areJidsSameUser } from '@whiskeysockets/baileys'
+var handler = async (m, { conn, participants, usedPrefix, command }) => {
+    if (!m.mentionedJid[0] && !m.quoted) {
+        return conn.reply(m.chat, '🚩 *Etiqueta o responde al mensaje de la persona que quieres eliminar*', m, rcanal );
+    }
 
-var handler = async (m, { conn, text, participants, args, command }) => {
+    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
 
-let member = participants.map(u => u.id)
-if(!text) {
-var sum = member.length
-} else {
-var sum = text} 
-var total = 0
-var sider = []
-for (let i = 0; i < sum; i++) {
-let users = m.isGroup ? participants.find(u => u.id == member[i]) : {}
-if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) { 
-if (typeof global.db.data.users[member[i]] !== 'undefined'){
-if (global.db.data.users[member[i]].whitelist == false){
-total++
-sider.push(member[i])}
-}else {
-total++
-sider.push(member[i])}}}
-const delay = time => new Promise(res=>setTimeout(res,time))
+    const groupInfo = await conn.groupMetadata(m.chat);
+    const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
+    const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
+    //const nn = conn.getName(m.sender);
 
-switch (command) {
+    if (user === conn.user.jid) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar el bot del grupo', m, rcanal, );
+    }
 
-case 'fantasmas': 
-if(total == 0) return conn.reply(m.chat, `🎌 *Este grupo es activo, no tiene fantasmas*`, m, fake, ) 
-m.reply(`🐉 *Revisión de inactivos*\n\n⚠️ *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:*\nEsto no es al 100% acertado, el bot inicia el conteo de mensajes a partir de que se active en este número`, null, { mentions: sider }) 
-break
+    if (user === ownerGroup) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del grupo', m, rcanal, );
+    }
 
-case 'kickfantasmas':  
-if(total == 0) return conn.reply(m.chat, `🎌 *Este grupo es activo no tiene fantasmas*`, m, fake, ) 
-await m.reply(`🎉 *Eliminación de inactivos*\n\n⚠️ *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n❗ _El bot eliminara a los usuarios de la lista mencionada cada 10 segundos_`, null, { mentions: sider }) 
-await delay(1 * 10000)
-let chat = global.db.data.chats[m.chat]
-chat.welcome = false
-try {
+    if (user === ownerBot) {
+        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del bot', m, rcanal );
+    }
 
-let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
-let kickedGhost = sider.map(v => v.id).filter(v => v !== conn.user.jid)
-for (let user of users)
-if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin)
-{
-let res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-kickedGhost.concat(res)
-await delay(1 * 10000)
-}} finally{
-chat.welcome = true
-}
-break            
-}
+    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-}
-handler.tags = ['grupo']
-handler.command = ['fantasmas', 'kickfantasmas']
-handler.group = true
-handler.botAdmin = true
-handler.admin = true
-handler.fail = null
+//conn.reply('573012482597@s.whatsapp.net', `🚩 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
+};
 
-export default handler
+handler.help = ['kick'];
+handler.tags = ['grupo'];
+handler.command = ['kick','echar','hechar','sacar','ban'];
+handler.admin = true;
+handler.group = true;
+handler.register = true
+handler.botAdmin = true;
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+export default handler;
