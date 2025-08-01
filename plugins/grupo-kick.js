a67f402b0386747848540e0bcf34ddc5035df725
Dioneibi-rip <fandubdreams@gmail.com>
2025-07-31 22:10:23 -0400

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,13 +1,13 @@
 
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `🚩 *Etiqueta o responde al mensaje de la persona que quieres eliminar`
+let kickte = `✳️ Uso correcto del comando.\n*${usedPrefix + command}* @tag`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐝𝐨 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨) 
+m.reply(`✅ ᴜꜱᴜᴀʀɪᴏ ᴇʟɪᴍɪɴᴀᴅᴏ ᴅᴇʟ ɢʀᴜᴘᴏ `) 
 
 }
 
