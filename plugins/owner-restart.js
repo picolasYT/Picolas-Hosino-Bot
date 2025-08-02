4fba2e1142435c73626efc14370639dbf881a432
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-08-02 00:47:17 -0700

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
     try {
-        m.reply('「❀」 Reiniciando El Bot....')
+        m.reply('「🪷」 Reiniciando la Bot....')
         setTimeout(() => {
             process.exit(0)
         }, 3000) 
