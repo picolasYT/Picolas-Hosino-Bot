5826d08c02b57660f8697d49118fc23c470b6581
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-02 14:26:40 -0400

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -75,6 +75,7 @@ global.db.data.users[m.sender].Subs = new Date * 1
 } 
 handler.help = ['qr', 'code']
 handler.tags = ['serbot']
+handler.private = true
 handler.command = ['qr', 'code']
 export default handler 
 
