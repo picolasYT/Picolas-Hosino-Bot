fcfc55379107fc16965c2f25766de176001704b1
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-14 16:53:46 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -238,9 +238,13 @@ export async function handler(chatUpdate) {
                         return
                     }
                     if (user.antispam2 && isROwner) return
+
+                    /*
                     let time = global.db.data.users[m.sender].spam + 3000
                     if (new Date - global.db.data.users[m.sender].spam < 3000) return console.log(`[ SPAM ]`) 
                     global.db.data.users[m.sender].spam = new Date * 1
+                    */
+
                     if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
                         let chat = global.db.data.chats[m.chat]
                         let user = global.db.data.users[m.sender]
