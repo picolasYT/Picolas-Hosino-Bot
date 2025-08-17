fbd0dbf1410701b2583a91528cc07add6e34ab49
Dioneibi-rip <fandubdreams@gmail.com>
2025-08-17 19:02:37 -0400

    Actualizar grupo-revoke.js

--- plugins/grupo-revoke.js
+++ plugins/grupo-revoke.js
@@ -1,15 +1,15 @@
 var handler = async (m, { conn }) => {
+  let res = await conn.groupRevokeInvite(m.chat);
+  let newLink = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat);
+  
+  conn.reply(m.chat, `{emoji2} *Nuevo enlace del grupo:*\n${newLink}`, m);
+};
 
-let res = await conn.groupRevokeInvite(m.chat)
-let gruf = m.chat
-conn.reply(m.sender, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gruf), m)
-}
+handler.help = ['revoke'];
+handler.tags = ['grupo'];
+handler.command = ['revoke', 'restablecer'];
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
 
-handler.help = ['revoke']
-handler.tags = ['grupo']
-handler.command = ['revoke', 'restablecer']
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-
-export default handler
+export default handler;
